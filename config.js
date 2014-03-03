0c19a4f630c5e0cd8b9032438dd04950d864e5a0
Juho H <juhoth@ovi.com>
2014-03-04 01:27:31 +0200

    add mongolab mongo instance

--- config.js
+++ config.js
@@ -11,5 +11,6 @@ MODELDIR = path.join(BASEDIR, 'models');
 DBDIR = path.join(BASEDIR, 'db');
 MONGOURI = process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
+    'mongodb://elk:humongous@ds031359.mongolab.com:31359/blogoose' ||
     'mongodb://localhost/blogoose';
 
