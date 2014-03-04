4bbe049705fd4b73de253f225f21e4e8f6e83bbb
Juho H <juhoth@ovi.com>
2014-03-04 12:38:18 +0200

    edit n update posts

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ mongoose.connect(MONGOURI, function (err, res) {
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.bodyParser());
+app.use(express.methodOverride());
 app.use(express.static(__dirname + '/public'));
 
 /**
@@ -57,6 +58,35 @@ app.post('/posts', function createPost(req, res) {
             res.render('/posts/new.jade');
         } else {
             res.redirect('/');
+        }
+    });
+});
+
+app.get('/posts/:id/edit', function editPost(req, res) {
+    Post.findOne({ _id: req.params.id }, function (err, post) {
+        if (err) res.send(err);
+        else res.render('posts/edit.jade', { post: post });
+    });
+});
+
+app.put('/posts/:id', function updatePost(req, res) {
+    var id = req.param('id');
+    var postParams = req.body.post;
+    // Extract to Post.update(id, postParams);
+    Post.findOne({ _id: id }, function (err, post) {
+        if (err) {
+            res.send(err);
+        } else {
+            for (var key in postParams) {
+                post[key] = postParams[key];
+            }
+            post.save(function (err, post) {
+                if (err) {
+                    res.render('posts/edit.jade', { post: post });
+                } else {
+                    res.redirect('/');
+                }
+            });
         }
     });
 });
